4e012442ccdc658a372b9ff674f3d4daac27ae03
MohammadAkbar <makba002@ucr.edu>
2019-10-26 16:41:48 -0700

    syntax

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, Navigation
+from flask import Flask, render_template, url_for
+from flask.ext.navigation import Navigation
 
 app = Flask(__name__)
 nav = Navigation(app)
