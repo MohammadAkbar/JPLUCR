dbc3d57f766f59d448e7eee6db970877733517af
MohammadAkbar <makba002@ucr.edu>
2019-10-26 16:38:21 -0700

    navigation bar

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask.ext.navigation import Navigation
 
 app = Flask(__name__)
+nav = Navigation(app)
+nav.Bar('top',[
+    nav.Item('Home', 'index'),
+    nav.Item('View Dataset', 'viewData' , {'id':1},
+             ])
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/viewData/<int:id>')
+def viewData(id):
+    return render_template('news.html', id = id)
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
