582d260756aecb808f611ccce740f9cebe04f35c
MohammadAkbar <makba002@ucr.edu>
2019-11-06 10:59:27 -0800

    new

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -33,5 +33,5 @@ def dataClassify():
     return render_template('dataClassify.html', title='Data Classify')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,threaded=True,debug=True)
+    app.run(host='0.0.0.0',port=80,threaded=True,debug=False, ssl_context=('cert.pem', 'key.pem'))
     #app.run()
