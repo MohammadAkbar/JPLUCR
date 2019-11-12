365cdc69b283cf795ef3873888d34b0f885bcc5a
MohammadAkbar <makba002@ucr.edu>
2019-11-11 21:43:03 -0800

    new

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -37,7 +37,9 @@ def index():
     if not google.authorized:
         return redirect(url_for("google.login"))
     resp = google.get("/oauth2/v2/userinfo")
-    return render_template('dataVisual.html' , title='Data Visualization',email=resp.json()["email"])
+    assert resp.ok, resp.text
+    return "You are @{login} with google".format(login=resp.json()["email"])
+    #return render_template('dataVisual.html' , title='Data Visualization',resp.json()["email"])
 
 @app.route('/dataVisual')
 def dataVisual():
