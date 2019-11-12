2f4c0bc5d8c582e23acd1c6ec01b1e701d59be53
MohammadAkbar <makba002@ucr.edu>
2019-11-11 19:03:22 -0800

    new

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,11 +1,10 @@
 from __future__ import print_function
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import Flask, render_template, url_for, request, jsonify, redirect, Blueprint
 from flask_navigation import Navigation
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 import json
 import os
-
 import sys
 
 app = Flask(__name__)
@@ -18,6 +17,34 @@ nav.Bar('top',[
     nav.Item('Data Visualization', 'dataVisual'),
     nav.Item('Classify Data', 'dataClassify')
 ])
+
+from flask_dance.contrib.google import make_google_blueprint, google
+app.secret_key = "supersekrit"  # Replace this with your own secret!
+
+blueprint = make_google_blueprint(
+    client_id="1045884176358-7ujjr3afn72o8fpatg58gvtkbf48s9o7.apps.googleusercontent.com",
+    client_secret="wxpbWHrKpva3SsPbeobgzLZK",
+    scope=['profile', 'email'],
+    reprompt_consent=False,
+    reprompt_select_account=False,
+    redirect_url=None,
+    redirect_to=None,
+    login_url=None,
+    authorized_url=None,
+    session_class=None,
+    storage=None,
+    hosted_domain=None,
+    offline=True
+)
+app.register_blueprint(blueprint, url_prefix="/login")
+
+@app.route("/google_login")
+def google_login():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    return resp.text
 
 @app.route('/')
 def index():
